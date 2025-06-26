bd446a35e53d55786c753873f0f50ad1f902efbf
shubham kondewar <shubham.kondewar98@gmail.com>
2025-06-26 19:15:32 +0530

    training pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -53,16 +53,6 @@ class DataIngestion:
             raise CustomException(e,sys) 
             
         
-### RUN FILE 
-
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
-
-
-
 
 
 
